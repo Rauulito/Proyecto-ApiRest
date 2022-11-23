5279262355e6301ab87b38d00904676e87872b28
Rauulito <7rnavarro7@gmail.com>
2022-11-23 13:06:39 +0100

    actualizando actualizada

--- Gestor_Clientes-main/Api-Rest/actualizandoclientes.py
+++ Gestor_Clientes-main/Api-Rest/actualizandoclientes.py
@@ -7,6 +7,12 @@ import database as db
 
 headers = {"content-type": "charset=utf-8"}
 
+class ModeloCliente(BaseModel):
+    dni: constr(min_length=3, max_length=3)
+    nombre: constr(min_length=2, max_length=30)
+    apellido: constr(min_length=2, max_length=30)
+
+
 @ app.put("/clientes/actualizar/", tags=["Clientes"])
 async def clientes_actualizar(datos: ModeloCliente):
     if db.Clientes.buscar(datos.dni):
@@ -14,3 +20,60 @@ async def clientes_actualizar(datos: ModeloCliente):
         if cliente:
             return JSONResponse(content=cliente.to_dict(), headers=headers)
     raise HTTPException(status_code=404)
+
+{
+  "dni": "15J",
+  "nombre": "Victoria",
+  "apellido": "Pérez"
+}
+
+{
+  "detail": [
+    {
+      "loc": [
+        "body",
+        "dni"
+      ],
+      "msg": "ensure this value has at least 3 characters",
+      "type": "value_error.any_str.min_length",
+      "ctx": {
+        "limit_value": 3
+      }
+    },
+    {
+      "loc": [
+        "body",
+        "nombre"
+      ],
+      "msg": "ensure this value has at least 2 characters",
+      "type": "value_error.any_str.min_length",
+      "ctx": {
+        "limit_value": 2
+      }
+    },
+    {
+      "loc": [
+        "body",
+        "apellido"
+      ],
+      "msg": "ensure this value has at least 2 characters",
+      "type": "value_error.any_str.min_length",
+      "ctx": {
+        "limit_value": 2
+      }
+    }
+  ]
+}
+
+{
+  "detail": [
+    {
+      "loc": [
+        "body",
+        "dni"
+      ],
+      "msg": "Cliente ya existente o DNI incorrecto",
+      "type": "value_error"
+    }
+  ]
+}

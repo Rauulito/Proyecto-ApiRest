11681e30cb192b0d1939a583e94c9d921a5f5f54
lorenzoo22 <lagudmarr@gmail.com>
2022-11-23 12:24:06 +0100

    paso_2

--- Gestor_Clientes-main/Api-Rest/api.py
+++ Gestor_Clientes-main/Api-Rest/api.py
@@ -1,1 +1,19 @@
-print("Servidor de la API.....")
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+@app.get("/")
+async def index():
+    content = {'mensaje': '¡Hola mundo!'}
+    return JSONResponse(content=content)
+
+# headers = {"content-type": "charset=utf-8"}
+    
+# @app.get("/")
+# async def index():
+#     content = {'mensaje': '¡Hola mundo!'}
+#     return JSONResponse(content=content, headers=headers)
+
+print("Servidor de la API...")
